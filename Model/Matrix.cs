f219d7085b8a294f21993c4d435095e7cd40f2f4
Thom-kauf <thomas_kauffman@outlook.com>
2022-12-21 14:57:27 -0900

    Set up making a grid. I might rework the whole thing though because right now it's super hacky. Looking into creating a navigation page for the calculation part of the process

---
+++ Model/Matrix.cs
@@ -0,0 +1,42 @@
+﻿namespace RowReductionAlgorithm;
+    
+/// <summary>
+/// This class will handle the logic for the algorithm
+/// </summary>
+public class Matrix
+{
+    //dimensions of the matrix
+    private int colNum;
+    private int rowNum;
+    private double[,] backingArray;
+
+    public Matrix ()
+    {
+
+    }   
+
+    /// <summary>
+    /// property for columns of the matrix
+    /// </summary>
+    public int ColNum
+    {
+        get { return colNum; }
+        set { colNum = value; }
+    }
+    /// <summary>
+    /// property for columns of the matrix
+    /// </summary>
+    public int RowNum
+    {
+        get { return rowNum; }
+        set { rowNum = value; }
+    }
+    /// <summary>
+    /// property for setting the backing array and accessing
+    /// </summary>
+    public double[,] BackingArray
+    {
+        get { return backingArray; }
+        set { backingArray = value; }
+    }
+}
--- RowReductionAlgorithm/MainPage.xaml.cs
+++ RowReductionAlgorithm/MainPage.xaml.cs
@@ -1,17 +1,146 @@
-﻿namespace RowReductionAlgorithm;
+﻿
+
+namespace RowReductionAlgorithm;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	private Matrix matrix;
 
 	public MainPage()
 	{
 		InitializeComponent();
+		matrix = new Matrix();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	/// <summary>
+	/// Sets column dimension for the matrix. Doesn't follow perfect mvc, but it would be really annoying to do this the 
+	/// "right" way
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
+	private void SetColNum(object sender, EventArgs e)
+	{
+		int value = -1;
+
+		if (!int.TryParse(colNum.Text, out value))
+			DisplayAlert("Invalid Input", "Value must be in the set of natural numbers", "cancel");
+
+		//not sure about this
+		else if (value < 1)
+			DisplayAlert("Invalid Input", "Value must be in the set of natural numbers", "cancel");
+
+		else
+			matrix.ColNum = value;
+
+	}
+	/// <summary>
+	/// Doesn't follow perfect mvc, but it would be really annoying to do this the 
+	/// "right" way
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
+	private void SetRowNum(object sender, EventArgs e)
+	{
+		int value = -1;
+
+		if (!int.TryParse(rowNum.Text, out value))
+			DisplayAlert("Invalid Input", "Value must be in the set of natural numbers", "cancel");
+
+		else if (value < 1)
+			DisplayAlert("Invalid Input", "Value must be in the set of natural numbers", "cancel");
+
+		else
+			matrix.RowNum = value;
+
+
+
+	}
+
+	private void SetDimensions(object sender, EventArgs e)
+	{
+		if (matrix.RowNum > 0 && matrix.ColNum > 0)
+		{
+			HSL.Clear();
+            MakeGrid();
+        }
+
+		else
+		{
+			DisplayAlert("Invalid Input", "Enter in your desired dimensions and try again", "cancel");
+
+		}
+	}
+
+	/// <summary>
+	/// Makes a grid of entries that is rowNum rows by colNum columns
+	/// </summary>
+	private void MakeGrid()
 	{
 
+		List<Entry> entries = new List<Entry>();
+
+        for (int i = 0; i < matrix.RowNum; i++)
+        {
+			HorizontalStackLayout hsl = new();
+			vsl.Add(hsl);
+			for (int j = 0; j < matrix.ColNum; j++)
+			{
+				Entry entry = new Entry();
+				hsl.Add(entry);
+
+				
+				entries.Add(entry);
+			}
+        }
+
+
+		HorizontalStackLayout hsl2 = new();
+		vsl.Add(hsl2);
+
+		
+
+		Button refButton = new();
+		refButton.Text = "REF that shit";
+
+		Button reffButton = new();
+		reffButton.Text = "REFF that shit";
+
+		hsl2.Add(reffButton);
+		hsl2.Add(refButton);
+
+		//set up the handlers for the different buttons
+		reffButton.Clicked += BeginReff;
+		refButton.Clicked  += BeginRef;
+
+
+
+        //boolean to keep track of if the next step needs to be commenced
+        int neccessarryFilledEntries = matrix.RowNum * matrix.ColNum;
+		int filledEntries = 0;
+		//while all the entries are NOT filled, keep checking
+        while (filledEntries < neccessarryFilledEntries)
+        {
+			foreach(Entry entry in entries)
+			{
+				entry.TextChanged
+			}
+        }
+
+
+
+
+
+
+    }
+
+	private void BeginReff(object sender, EventArgs e)
+	{
+		DisplayAlert("yes", "yues", "yes");
 	}
+
+    private void BeginRef(object sender, EventArgs e)
+    {
+        DisplayAlert("no", "no", "no");
+    }
 }
 
